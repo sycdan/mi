acc787af9e693733f3b3340cc07614b3ba93bf6a
Dan Stace <dstace@gmail.com>
2026-02-20 00:10:31 -0500

    add rsa pubkey

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cp "$SCRIPT_DIR"/git/config_identity-template ~/.gitconfig
 cp "$SCRIPT_DIR"/tmux/conf ~/.tmux.conf
 
-echo "Don't forget to set your email address in ~/.gitconfig"
+echo "Don't forget to set your email address in ~/.gitconfig if this is a company env!"
